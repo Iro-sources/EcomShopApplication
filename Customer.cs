b2e5865e93b878bcd3acd212df9b2d98ceedf1fd
Iro-sources <mohamediro98@gmail.com>
2026-06-18 03:13:26 +0200

    Initial project files

---
+++ Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcomShopApplication
+{
+    internal class Customer
+    {
+        private string name;
+        private string cart;
+        private int orders;
+
+        public Customer(string name, string cart)
+        {
+            this.name = name;       
+            this.cart = cart;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using System;
+Console.WriteLine("Hello Worlds");
+int result = 1 + 3 * 5;
+Console.WriteLine(result);
